131af7b2cc8c2b42c0b73ae45e6e34a0cc8b59a6
hiroaki.komori@gmail.com <KliMt1918>
2023-07-26 10:43:08 +0900

    toFullWidth

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function SetValue()
 	setsumei = tangoList[idx].意味;
 
 	var t = document.getElementById("Tango");
-	t.innerHTML = tango;
+	t.innerHTML =toFullWidth(tango);
 	var s = document.getElementById("Setsumei");
 	s.innerHTML = setsumei;
 }
@@ -34,5 +34,11 @@ async function loadJson() {
 	console.log(tangoList);
 	SetValue();
 }
+function toFullWidth(value) {
+	if (typeof value !== 'string') return value;
 
+	return String(value).replace(/[!-~]/g, function(all) {
+		return String.fromCharCode(all.charCodeAt(0) + 0xFEE0);
+	});
+};
 loadJson(); 
